cc344db07e1960c6dd3ff577290c38c19b76c658
veerkarri <veerreddykarri@gmail.com>
2021-06-19 10:37:45 +0100

    Commit

--- src/test/java/steps/BaseClass.java
+++ src/test/java/steps/BaseClass.java
@@ -1,8 +1,13 @@
 package steps;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseClass {
         public static ChromeDriver driver;
-    }
+}
 
